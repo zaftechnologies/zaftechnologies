9fc73ba2074f0bb1f8ab3d042ef1ecbf79c649b7
zaftechnologies <talha.jilal@zaftech.ca>
2024-12-03 13:29:50 -0500

    Create dwonload.ps1

---
+++ dwonload.ps1
@@ -0,0 +1,2 @@
+# Donwload office iso
+Invoke-WebRequest -Uri "https://us-east-1.console.aws.amazon.com/s3/object/zaftech-terraform?region=us-east-1&bucketType=general&prefix=SW_DVD5_Office_2016_64Bit_English_MLF_X20-42479.ISO" -OutFile "C:\office.iso"
